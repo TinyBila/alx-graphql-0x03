f833edce88b1e5052b0a90b24974816d26fdd150
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-10-25 21:38:49 +0200

    Update _app.tsx

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -11,3 +11,16 @@ export default function App({ Component, pageProps }: AppProps) {
     </ApolloProvider>
   )
 }
+
+import ErrorBoundary from '@/components/ErrorBoundary';
+import ErrorProneComponent from '@/components/ErrorProneComponent';
+
+const Home: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <ErrorProneComponent />
+    </ErrorBoundary>
+  );
+};
+
+export default Home;
